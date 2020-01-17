a949614e371194958fc22d444a61b3cc45715471
MaratSergievich <sergievich1911@gmail.com>
2020-01-17 20:07:17 +0300

    comment

---
+++ src/components/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () =>{
+  return(
+    <div className='content '>
+      <div>
+        <img src='https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+        <div>
+          post 3
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Profile;
