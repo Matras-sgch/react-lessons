58fe39d0365263b0b89be8eec0df0e783764f97b
MaratSergievich <sergievich1911@gmail.com>
2020-01-31 11:59:44 +0300

    add dialogs and a little bit refactoring

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import s from'./Profile.module.css';
 
 const Profile = () =>{
   return(
-    <div className={s.content}>
+    <div>
       <div>
         <img className = {s.img} src='https://i.mr-7.ru/photos/2019/11/6PfBp9Kgl9b2Fod4uo7j.jpg'/>
       </div>
